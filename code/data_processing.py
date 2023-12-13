2a9ee1c7a80737f4f958898953013c7b96daf6a4
Cody Myers <52272056+myerscody16@users.noreply.github.com>
2023-12-12 22:05:36 -0500

    Create source match data csv

--- code/data_processing.py
+++ code/data_processing.py
@@ -1,5 +1,5 @@
-from pandas import read_json
-
+import pandas as pd
+from io import StringIO
 
 class DataProcessor:
     def __init__(self, process_historic_data, season, raw_dir_path, source_data_dir_path):
@@ -14,10 +14,22 @@ class DataProcessor:
 
     
     def process_selected_season_data(self):
-        print(self.raw_directory_path + f'{self.season}_premier_league_match_data_raw.jsonl')
-        data = read_json(self.raw_directory_path + f'{self.season}_premier_league_match_data_raw.jsonl', lines=True)
-        print(data.columns)
-
+        with open(f'{self.raw_directory_path}{self.season}_premier_league_match_data_raw.jsonl', 'r') as file:
+            jsonl_string = file.read()
+        json_io = StringIO(jsonl_string)
+        data = pd.read_json(json_io, lines=True)
+        matches = pd.DataFrame({
+            'id': data['id'],
+            'date_played': data['utcDate'],
+            'home_team_id': data['homeTeam'].apply(lambda x: x.get('id')),
+            'home_team_name': data['homeTeam'].apply(lambda x: x.get('name')),
+            'away_team_id': data['awayTeam'].apply(lambda x: x.get('id')),
+            'away_team_name': data['awayTeam'].apply(lambda x: x.get('name')),
+            'winner': data['score'].apply(lambda x: x.get('winner')),
+            'home_goals': data['score'].apply(lambda x: x.get('fullTime').get('home')),
+            'away_goals': data['score'].apply(lambda x: x.get('fullTime').get('away'))
+        })
+        matches.to_csv(f'{self.source_directory_path}{self.season}_premier_league_match_data_matches.csv', sep=',')
 
     def extract_data(self):
         if self.process_historic_data:
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from code.data_processing import DataProcessor
 #### User defined variables
 
 # Minimum year of 2019 due to API constraints
-start_of_season_year = 2020
+start_of_season_year = 2023
 ingest_data = False
 process_data = True
 process_historic_data = False
@@ -14,7 +14,7 @@ process_historic_data = False
 
 if __name__ == '__main__':
     raw_dir_path = f'data/raw/'
-    source_data_dir_path = f'data/source/{start_of_season_year}_premier_league_match_data.jsonl'
+    source_data_dir_path = f'data/source/'
     if ingest_data:
         data_retriever = DataRetriever(start_of_season_year, raw_dir_path)
         data_retriever.getMatchData()
