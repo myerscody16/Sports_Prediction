e5e7223dfa5c9fc5aeea1392b1c6092778b2c2f9
Cody Myers <52272056+myerscody16@users.noreply.github.com>
2023-12-14 22:12:39 -0500

    Generalized code for processing and updated main for ingestion and processing

--- code/data_processing.py
+++ code/data_processing.py
@@ -1,19 +1,28 @@
 import pandas as pd
 from io import StringIO
 
+
 class DataProcessor:
-    def __init__(self, process_historic_data, season, raw_dir_path, source_data_dir_path):
+    def __init__(self, season, raw_dir_path, source_data_dir_path):
         self.season = season
         self.raw_directory_path = raw_dir_path
         self.source_directory_path = source_data_dir_path
-        self.process_historic_data = process_historic_data
 
 
-    def process_all_data(self):
-        pass
+    def form_value_calculation(self, form_string):
+        form_string = form_string.replace(',', '')
+        value_dict = {
+            'w': 1,
+            'l': -1,
+            'd': 0
+        }
+        form_value = 0
+        for char in form_string:
+            form_value += value_dict[char.lower()]
+        return form_value
 
     
-    def processStandingsData(self):
+    def process_standings_data(self):
         with open(f'{self.raw_directory_path}{self.season}_premier_league_standings_raw.jsonl', 'r') as file:
             jsonl_string = file.read()
         json_io = StringIO(jsonl_string)
@@ -21,6 +30,8 @@ class DataProcessor:
         for row in data.iterrows():
             if row[1]['type'] == 'TOTAL':
                 standings_df = pd.json_normalize(row[1]['table'])
+                standings_df['form_value'] = standings_df['form'].apply(lambda x: self.form_value_calculation(x))
+                standings_df.drop(['form', 'team.crest', 'team.shortName', 'team.tla'], axis = 1, inplace=True)
                 standings_df.to_csv(f'{self.source_directory_path}{self.season}_premier_league_standings.csv', sep=',')
 
 
@@ -41,10 +52,3 @@ class DataProcessor:
             'away_goals': data['score'].apply(lambda x: x.get('fullTime').get('away'))
         })
         matches.to_csv(f'{self.source_directory_path}{self.season}_premier_league_match_data_matches.csv', sep=',')
-
-
-    def extract_data(self):
-        if self.process_historic_data:
-            self.process_all_data()
-        else:
-            self.process_selected_season_data()
--- main.py
+++ main.py
@@ -1,23 +1,34 @@
 from code.data_ingestion import DataRetriever
 from code.data_processing import DataProcessor
+import os
+
 
 #### User defined variables
-
-# Minimum year of 2019 due to API constraints
+# Minimum year of 2020 due to API constraints
 start_of_season_year = 2023
 ingest_data = False
-process_data = True
-process_historic_data = False
-
+process_data = False
+process_historic_data = True
 ####
 
 
 if __name__ == '__main__':
-    raw_dir_path = f'data/raw/'
-    source_data_dir_path = f'data/source/'
+    raw_dir_path = 'data/raw/'
+    source_data_dir_path = 'data/source/'
     if ingest_data:
         data_retriever = DataRetriever(start_of_season_year, raw_dir_path)
         data_retriever.getMatchData()
+        data_retriever.getStandingsData()
     if process_data:
-        data_processor = DataProcessor(process_historic_data, start_of_season_year, raw_dir_path, source_data_dir_path)
+        data_processor = DataProcessor(start_of_season_year, raw_dir_path, source_data_dir_path)
         data_processor.extract_data()
+    if process_historic_data:
+        seasons = os.listdir(raw_dir_path)
+        match_seasons = [path.split('_')[0] for path in seasons if 'match' in path]
+        for season in match_seasons:
+            data_processor = DataProcessor(season, raw_dir_path, source_data_dir_path)
+            data_processor.process_selected_season_data()
+        standings_seasons = [path.split('_')[0] for path in seasons if 'standings' in path]
+        for season in standings_seasons:
+            data_processor = DataProcessor(season, raw_dir_path, source_data_dir_path)
+            data_processor.process_standings_data()
